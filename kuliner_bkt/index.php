b038313d7f6243919155d49a29027e1a9a408525
athifah27zahra <50130026+athifah27zahra@users.noreply.github.com>
2019-11-06 18:00:12 +0700

    Update index.php

--- kuliner_bkt/index.php
+++ kuliner_bkt/index.php
@@ -37,7 +37,7 @@ require("connect.php");
 
 <script type="text/javascript">
 
-var server = "http://13.76.226.54/mysql8/tourism_bkt2/kuliner_bkt/";
+var server = "http://13.76.226.54/mysql8/kuliner_bkt/";
 var map;
 var markersDua = [];
 var koordinat = 'null'
