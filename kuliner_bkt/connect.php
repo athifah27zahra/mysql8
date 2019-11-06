b2f1fda788c0e5a587e411eec55c4b3973c213a7
athifah27zahra <50130026+athifah27zahra@users.noreply.github.com>
2019-11-06 17:41:50 +0700

    Update connect.php

--- kuliner_bkt/connect.php
+++ kuliner_bkt/connect.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
 	$user = "root";
-	$pass = "seia";
+	$pass = "Akashiathifah1";
 	$dbname = "tumbal";
 	$conn = mysqli_connect($host, $user, $pass, $dbname) or die(mysqli_error());
 ?>
